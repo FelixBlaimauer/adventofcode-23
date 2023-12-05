7ca6da4839fde00d54c5a85be2aaa11241cdc398
Felix Blaimauer <blaimauer.felix@gmail.com>
2023-12-05 11:28:25 +0100

    Day 05

--- src/main/kotlin/day_05/day_05.kt
+++ src/main/kotlin/day_05/day_05.kt
@@ -6,29 +6,35 @@ enum class Conversions {
     SEED_TO_SOIL, SOIL_TO_FERTILIZER, FERTILIZER_TO_WATER, WATER_TO_LIGHT, LIGHT_TEMPERATURE, TEMPERATURE_TO_HUMIDITY, HUMIDITY_TO_LOCATION
 }
 
-data class ConversionMap(val destinationStart: Int, val rangeStart: Int, val rangeLength: Int) {
+data class ConversionMap(val destinationStart: Long, val rangeStart: Long, val rangeLength: Long) {
     companion object {
         fun parse(string: String): ConversionMap {
-            val (destination, start, length) = string.split(" ").map { it.toInt() }
+            val (destination, start, length) = string.split(" ").map { it.toLong() }
             return ConversionMap(destination, start, length)
         }
 
-        fun seedToLocation(seed: Int, maps: List<List<ConversionMap>>) = maps.fold(seed) { value, mapList ->
+        fun seedToLocation(seed: Long, maps: List<List<ConversionMap>>) = maps.fold(seed) { value, mapList ->
             val map = mapList.find { it.isInRange(value) }
 
             return@fold map?.convert(value) ?: value
         }
     }
 
-    fun isInRange(value: Int): Boolean = (value >= rangeStart) && value <= (rangeStart + rangeLength - 1)
+    fun isInRange(value: Long): Boolean = (value >= rangeStart) && value <= (rangeStart + rangeLength - 1)
 
-    fun convert(value: Int) = destinationStart + value - rangeStart
+    fun convert(value: Long) = destinationStart + value - rangeStart
 }
 
 fun main() {
     val input = File("src/main/resources/day_05/input.txt").readLines()
 
-    val wantedSeeds = input[0].split(": ")[1].split(" ").map { it.toInt() }
+    // Level 1
+    // val wantedSeeds = input[0].split(": ")[1].split(" ").map { it.toLong() }
+
+    // Level 2
+    // will fill your heap: val wantedSeeds = input[0].split(": ")[1].split(" ").map { it.toLong() }.chunked(2).flatMap {it.first() .. it.first() + it.last() }
+    val wantedSeeds = input[0].split(": ")[1].split(" ").map { it.toLong() }.chunked(2)
+
 
     val maps = mutableListOf<MutableList<ConversionMap>>()
 
@@ -42,8 +48,20 @@ fun main() {
         maps.last() += map
     }
 
-    val seedLocations = wantedSeeds.map { ConversionMap.seedToLocation(it, maps) }
+    var min = Long.MAX_VALUE
 
-    println(seedLocations)
-    println(seedLocations.min())
+    // Level 1
+    // val seedLocations = wantedSeeds.map { println("yo"); return@map ConversionMap.seedToLocation(it, maps) }
+
+    // Level 2 (takes a little bit to process)
+    for (seedRange in wantedSeeds) {
+        for (seed in seedRange.first()..seedRange.first() + seedRange.last()) {
+            val location = ConversionMap.seedToLocation(seed, maps)
+            if (location < min) {
+                min = location
+            }
+        }
+    }
+    
+    println(min)
 }
