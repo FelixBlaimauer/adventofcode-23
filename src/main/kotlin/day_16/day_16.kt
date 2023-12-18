56739b606c6420cac8354e999dde0c7d5c6df5c2
Felix Blaimauer <blaimauer.felix@gmail.com>
2023-12-18 13:55:21 +0100

    Day 16

--- src/main/kotlin/day_16/day_16.kt
+++ src/main/kotlin/day_16/day_16.kt
@@ -1,10 +1,10 @@
 package day_16
 
+import OpposingRectDirection
 import RectDirection
 import RectDirectionVectors
 import Vec2
 import getResourceFile
-import java.lang.Exception
 
 val MirrorDirections = mapOf(
     '/' to mapOf(
@@ -12,13 +12,16 @@ val MirrorDirections = mapOf(
         RectDirection.RIGHT to RectDirection.DOWN,
         RectDirection.DOWN to RectDirection.RIGHT,
         RectDirection.LEFT to RectDirection.UP
-    ),
-    '\\' to mapOf(
+    ), '\\' to mapOf(
         RectDirection.UP to RectDirection.RIGHT,
         RectDirection.RIGHT to RectDirection.UP,
         RectDirection.DOWN to RectDirection.LEFT,
         RectDirection.LEFT to RectDirection.DOWN
     )
+)
+
+val SplitterDirections = mapOf(
+    '-' to listOf(RectDirection.LEFT, RectDirection.RIGHT), '|' to listOf(RectDirection.UP, RectDirection.DOWN)
 )
 
 data class LightBeam(var position: Vec2, var direction: RectDirection) {
@@ -27,39 +30,71 @@ data class LightBeam(var position: Vec2, var direction: RectDirection) {
     }
 
     fun mirror(mirror: Char) {
-        direction = MirrorDirections[mirror]!![direction]!!
+        direction = MirrorDirections[mirror]!![OpposingRectDirection[direction]]!!
     }
 
-    fun split(splitter: Char) {
+    fun split(splitter: Char, grid: List<CharArray>, visited: MutableSet<LightBeam>): Set<Vec2>? {
+        if (direction in SplitterDirections[splitter]!!) return null
 
+        return SplitterDirections[splitter]!!.map { traceBeam(LightBeam(position, it), grid, visited) }
+            .reduce { a, b -> a + b }
     }
 }
 
-fun traceBeam(beam: LightBeam, grid: List<CharArray>, energized: MutableSet<Vec2> = mutableSetOf()): Set<Vec2> {
+fun traceBeam(
+    beam: LightBeam,
+    grid: List<CharArray>,
+    visited: MutableSet<LightBeam> = mutableSetOf(),
+    energized: MutableSet<Vec2> = mutableSetOf()
+): Set<Vec2> {
     while (true) {
         beam.move()
-        println(beam.position)
+        if (beam in visited || beam.position.y < 0 || beam.position.y >= grid.size || beam.position.x < 0 || beam.position.x >= grid[0].size) return energized
 
-        if (beam.position.y < 0 || beam.position.y >= grid.size || beam.position.x < 0 || beam.position.x >= grid[0].size)
-            return energized
+        energized += beam.position
+        visited += beam
 
         val tile = grid[beam.position.y][beam.position.x]
 
         when (tile) {
             '/', '\\' -> beam.mirror(tile)
-            '|', '-' -> throw Exception("Fuc")
-//            '|', '-' -> beam.split(tile)
+            '|', '-' -> {
+                val traced = beam.split(tile, grid, visited)
+                if (traced != null) return energized + traced
+            }
         }
-        energized += beam.position
     }
 }
 
 fun main() {
-    val input = getResourceFile("day_16/example2.txt").readLines().map{it.toCharArray()}
+    val input = getResourceFile("day_16/input.txt").readLines().map { it.toCharArray() }
+    // Level 1
+//    val light = LightBeam(Vec2(-1, 0), RectDirection.RIGHT)
 
-    val light = LightBeam(Vec2(-1, 0), RectDirection.RIGHT)
+//    val energized = traceBeam(light, input)
 
-    val energized = traceBeam(light, input)
-    println(energized)
-    println(energized.size)
+    // Level 2
+    val energized = mutableListOf<Int>()
+    for (y in input.indices) {
+        val beams =
+            listOf(LightBeam(Vec2(-1, y), RectDirection.RIGHT), LightBeam(Vec2(input[0].size, y), RectDirection.LEFT))
+
+        beams.forEach {
+            energized += traceBeam(it, input).size
+        }
+    }
+
+    for (x in input[0].indices) {
+        val beams = listOf(
+            LightBeam(Vec2(x, -1), RectDirection.DOWN), LightBeam(Vec2(x, input.size), RectDirection.UP)
+        )
+
+        beams.forEach {
+            energized += traceBeam(it, input).size
+        }
+    }
+
+
+//    println(energized)
+    println(energized.max())
 }
